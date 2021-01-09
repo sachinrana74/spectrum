72f187a965cf3641fd3a59362346ca451838484b
sachinrana74 <sachinkumarrana7420@gmail.com>
2021-01-09 16:32:45 +0530

    Some changes related to alignment

--- wedding_gallery.php
+++ wedding_gallery.php
@@ -64,34 +64,34 @@
                 </div>
             </div>
         </div>
-        <div class="container-fluid p-0">
+        <div class="container-fluid p-1">
             <div class="row no-gutters grid">
-                        <div class="single_gallery grid-item large_img ">
+                        <div class="single_gallery grid-item col-xl-4 col-md-4 p-2 ">
                                 <a class="img-pop-up  thumb" href="image/wedding/1.jpg" >
                                     <img src="image/wedding/1.jpg" alt="">
                                 </a>
                             </div>
-                        <div class="single_gallery grid-item mid_img">
+                        <div class="single_gallery grid-item col-xl-4 col-md-4 p-2">
                                 <a class="img-pop-up thumb" href="image/wedding/2.jpg" >
                                     <img src="image/wedding/2.jpg" alt="">
                                 </a>
                             </div>
-                        <div class="single_gallery grid-item small_img">
+                        <div class="single_gallery grid-item col-xl-4 col-md-4 p-2">
                                 <a class="img-pop-up thumb" href="image/wedding/3.jpg" >
                                     <img src="image/wedding/3.jpg" alt="">
                                 </a>
                             </div>
-                        <div class="single_gallery grid-item small_img">
+                        <div class="single_gallery grid-item col-xl-4 col-md-4 p-2">
                                 <a class="img-pop-up thumb" href="image/wedding/4.jpg" >
                                     <img src="image/wedding/4.jpg" alt="">
                                 </a>
                             </div>
-                        <div class="single_gallery grid-item large_img">
+                        <div class="single_gallery grid-item col-xl-4 col-md-4 p-2">
                                 <a class="img-pop-up thumb" href="image/wedding/5.jpg" >
                                     <img src="image/wedding/5.jpg" alt="">
                                 </a>
                             </div>
-                        <div class="single_gallery grid-item mid_img">
+                        <div class="single_gallery grid-item col-xl-4 col-md-4 p-2">
                                 <a class="img-pop-up thumb" href="image/wedding/6.jpg" >
                                     <img src="image/wedding/6.jpg" alt="">
                                 </a>
